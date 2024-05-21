b59bc2345481a87d968af8919c2cb2e360dd13de
Alexey Nebolsin <alexey.nebolsin.jr@gmail.com>
2024-05-21 06:59:24 +0500

    Add an "API started" notification

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ import './routes'
 import { globalPino } from "./constants"
 import { updateLatestConfirmedBlockLoop } from "./network"
 import { checkRelayerStateLoop } from "./energy"
+import { sendTelegramNotification } from "./notifications"
 
 const port = Number(process.env.PORT ?? '3000')
 const host = process.env.HOST ?? '0.0.0.0'
 
 updateLatestConfirmedBlockLoop(globalPino)
 checkRelayerStateLoop(globalPino)
+sendTelegramNotification('API started', globalPino)
 
 // Run the server!
 app.listen({ port, host }, function (err) {
